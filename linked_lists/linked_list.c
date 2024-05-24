58f22dbec627f4bc191b03294c9849e8ffba7f96
Mahadev Panchal <upvord@gmail.com>
2024-05-24 18:16:17 +0530

    Create node

--- linked_lists/linked_list.c
+++ linked_lists/linked_list.c
@@ -7,44 +7,36 @@ struct node {
   struct node *link;
 };
 
+struct node *create_node(int n) {
+  struct node *n1 = (struct node *)malloc(1 * sizeof(struct node));
+  if (n1 == NULL) {
+    return NULL;
+  }
+  n1->data = n;
+  n1->link = NULL;
+
+  return n1;
+}
+
 int main(int argc, char *argv[]) {
   struct node *head = NULL;
   struct node *t = NULL;
 
-  struct node *n1 = (struct node *)malloc(1 * sizeof(struct node));
-  n1->data = 10;
-  n1->link = NULL;
-
-  head = n1;
-  t = head;
-
-  n1 = (struct node *)malloc(1 * sizeof(struct node));
-  n1->data = 20;
-  n1->link = NULL;
-
-  t->link = n1;
-  t = n1;
-
-  n1 = (struct node *)malloc(1 * sizeof(struct node));
-  n1->data = 30;
-  n1->link = NULL;
-
-  t->link = n1;
-  t = n1;
-
-  n1 = (struct node *)malloc(1 * sizeof(struct node));
-  n1->data = 40;
-  n1->link = NULL;
-
-  t->link = n1;
-  t = n1;
-
-  n1 = (struct node *)malloc(1 * sizeof(struct node));
-  n1->data = 50;
-  n1->link = NULL;
-
-  t->link = n1;
-  t = n1;
+  int i = 0;
+  int n = 10;
+  while (i < n) {
+    t = create_node(i);
+    if (head == NULL) {
+      head = t;
+    } else {
+      struct node *temp = head;
+      while (temp->link != NULL) {
+        temp = temp->link;
+      }
+      temp->link = t;
+    }
+    i++;
+  }
 
   t = head;
   while (t != NULL) {
